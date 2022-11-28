6003ae7c3a2528c29fb3aae8e4fb0b0706e48a90
isakpulkki <isak@pulkki.me>
2022-11-28 19:40:07 +0200

    Koriin voi lisätä yhden tavaran

--- Viikko4/tdd-ostoskori/src/ostoskori.py
+++ Viikko4/tdd-ostoskori/src/ostoskori.py
@@ -3,21 +3,25 @@ from ostos import Ostos
 
 class Ostoskori:
     def __init__(self):
-        self.tuotteet = []
+        self.ostokset = []
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
 
     def tavaroita_korissa(self):
-        return len(self.tuotteet)
+        return len(self.ostokset)
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
 
     def hinta(self):
-        return 0
+        hinta = 0
+        for ostos in self.ostokset:
+            hinta += ostos
+
+        return hinta
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
-        self.tuotteet.append(Tuote)
+        self.ostokset.append(Tuote)
         pass
 
     def poista_tuote(self, poistettava: Tuote):

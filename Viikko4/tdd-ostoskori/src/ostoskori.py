0e218f6e0939b4aaf17f99bf880548d7d63d7520
isakpulkki <isak@pulkki.me>
2022-11-28 21:06:00 +0200

    Kaksi samaa tuotetta, ostoskorissa vain yksi ostos

--- Viikko4/tdd-ostoskori/src/ostoskori.py
+++ Viikko4/tdd-ostoskori/src/ostoskori.py
@@ -7,7 +7,11 @@ class Ostoskori:
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
 
     def tavaroita_korissa(self):
-        return len(self.ostokset)
+        
+        maara = 0
+        for ostos in self.ostokset:
+            maara += ostos.lukumaara()
+        return maara 
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
@@ -21,6 +25,10 @@ class Ostoskori:
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
+        for ostos in self.ostokset:
+            if ostos.tuote == lisattava:
+                ostos.muuta_lukumaaraa(1)
+                return
         self.ostokset.append(Ostos(lisattava))
 
     def poista_tuote(self, poistettava: Tuote):
@@ -31,7 +39,7 @@ class Ostoskori:
         pass
         # tyhjentää ostoskorin
 
-    def ostokset(self):
-        pass
+    def ostoslista(self):
+        return self.ostokset
         # palauttaa listan jossa on korissa olevat ostos-oliot
         # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
--- Viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ Viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -67,3 +67,10 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(kana)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_ostoskori_sisaltaa_yhden_ostoksen(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        maara = len(self.kori.ostoslista())
+        self.assertEqual(maara, 1)

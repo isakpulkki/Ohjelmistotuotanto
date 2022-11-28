46d68688ce1e691f52237797427c9e31d98bb5d8
isakpulkki <isak@pulkki.me>
2022-11-28 21:07:23 +0200

    Kahden saman tuotteen lisäämisen jälkeen ostoskorissa ostos samalla nimellä, ja lukumäärä on 2

--- Viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ Viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -74,3 +74,11 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         maara = len(self.kori.ostoslista())
         self.assertEqual(maara, 1)
+
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_ostoskori_sisaltaa_ostoksen_jolla_sama_nimi_kuin_tuotteella_ja_niita_on_2(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito) 
+        ostos = self.kori.ostoslista()[0]
+        self.assertEqual(ostos.tuotteen_nimi(), "Maito")
+        self.assertEqual(ostos.lukumaara(), 2)

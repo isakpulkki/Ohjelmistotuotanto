582779f5fc881076bbbfbfa1b6013b57ca9acf18
isakpulkki <isak@pulkki.me>
2022-11-28 20:41:05 +0200

    Yhden tuotteen lisäämisen jälkeen korissa ostosolio

--- Viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ Viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -41,3 +41,15 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_ostoskorin_hinta_on_2x_tuotteen_hinta(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        self.assertEqual(self.kori.hinta(), 6)
+
+    def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostosolio(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        ostokset = self.kori.ostokset
+        self.assertEqual(ostokset[0].tuote, maito)
